07fad00af90a42355abd68bc8296768cca840036
tomassjt <tomasjtome@gmail.com>
2022-03-27 16:20:32 -0300

    Generamos un factory y rellenamos tabla de productos

---
+++ practica1/database/factories/productosFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\productos;
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class productosFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'nombre'=>$this->faker->sentence(),
+            'descripcion'=>$this->faker->sentence(),
+            'categoria'=>$this->faker->sentence(),
+            'precioCompra'=>$this->faker->biasedNumberBetween(0, 20),
+            'precioVenta'=>$this->faker->biasedNumberBetween(0, 20)  
+        ];
+    }
+}
---
+++ practica1/database/migrations/2022_03_27_181255_productos.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nombre');
+            $table->text('descripcion');
+            $table->text('categoria');
+            $table->decimal('precioCompra','5','2');
+            $table->decimal('precioVenta','5','2');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('productos');
+    }
+};
