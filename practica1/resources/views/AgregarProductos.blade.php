d660d652a69f5b16acf40801445008ac57c0b338
tomassjt <tomasjtome@gmail.com>
2022-03-30 23:26:51 -0300

    add validate

--- practica1/resources/views/AgregarProductos.blade.php
+++ practica1/resources/views/AgregarProductos.blade.php
@@ -12,23 +12,40 @@
 
 <label>Nombre</label>    
 <input type="text" name="nombre">
+ 
+@error('nombre')
+<small>*{{$message}}</small>
+@enderror
 <br>
 <label>Descripcion</label>    
 <textarea name="descripcion" cols="35" rows="10"></textarea>
+@error('descripcion')
+<small>*{{$message}}</small>
+@enderror
 <br>
 <label>Categoria</label>
 <input type="text" name="categoria">
+@error('categoria')
+<small>*{{$message}}</small>
+@enderror
 <br>
 <label>Precio Compra</label>
 <input type="number" name="precioCompra">
+@error('precioCompra')
+<small>*{{$message}}</small>
+@enderror
 <br>
 <label>Precio Venta</label>
 <input type="number" name="precioVenta">
-
+@error('precioVenta')
+<small>*{{$message}}</small>
+@enderror
 <br>
 <label>Cantidad</label>
 <input type="number" name="stock">
-
+@error('stock')
+<small>*{{$message}}</small>
+@enderror
 <br>
 <button type="submit">Agregar Producto</button>
 </form> 
